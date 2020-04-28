98db5575fa00665bd556e61b0c7aaad24be4a679
Patrik Näsström <patrik_nasstrom@hotmail.com>
2020-04-28 18:36:24 +0200

    updating user.actions and user.reducer with user.types and added redirect from signin  if sined in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import './App.css';
 
@@ -41,7 +41,13 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAdnSignUpPage} />
+          <Route exact path='/signin' render={()=> 
+            this.props.currentUser ? (
+            <Redirect to='/'/>
+            ) : (
+            <SignInAdnSignUpPage/>
+            )
+            } />
 
         </Switch>
       </div>
@@ -50,7 +56,15 @@ class App extends React.Component {
   }
 
 }
+const mapStateToProps = state =>({
+  currentUser: state.user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps )(App);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(App)
